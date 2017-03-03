7bb51a81b2e1fed2f8fb633971d3cf9af955dbec
Stephen Fluin <stephen.fluin@gmail.com>
2017-03-03 07:41:57 -0800

    Add components and routes live coding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'stats', component: StatsComponent},
+      {path: 'about', loadChildren: './about/about.module#AboutModule'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
